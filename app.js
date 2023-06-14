6990971f4fa7196914c093435c59bf472df68e26
Agung Rosyandi <agungrosyandi@gmail.com>
2023-06-15 02:09:47 +0700

    revisi narasihistorian fix v1

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({
-  default: { duration: 0.75, ease: "Power3.easeOut" },
-});
-
-// navbar -----------------------------------------------------
-
-const showAnim = gsap
-  .from("nav", {
-    yPercent: -100,
-    paused: true,
-    duration: 0.2,
-  })
-  .progress(1);
-
-ScrollTrigger.create({
-  start: "top top",
-  end: 99999,
-  onUpdate: (self) => {
-    self.direction === -1 ? showAnim.play() : showAnim.reverse();
-  },
-});
-
 // navbar burger toggle tab & mobile version -----------------------------------------------------
 
 const burger = document.querySelector(".burger");
@@ -40,163 +18,209 @@ links.forEach((link) => {
   });
 });
 
-// home sections
+// matchmedia responsive --------------------------------------------------
 
-tl.fromTo(
-  ".cta-home1",
-  { y: "-50%", opacity: 0 },
-  { y: 0, opacity: 1, duration: 2.5 }
-)
-  .fromTo(
-    ".cta-home2",
-    { y: "50%", opacity: 0 },
-    { y: 0, opacity: 1, duration: 2.5 },
-    "<35%"
-  )
-  .fromTo(
-    ".cta-home3",
-    { x: "50%", opacity: 0 },
-    { x: 0, opacity: 1, duration: 2.5 },
-    "<35%"
-  )
-  .fromTo(
-    ".cta-button",
-    { x: "50%", opacity: 0 },
-    { x: 0, opacity: 1, duration: 2.5 },
-    "<35%"
-  )
-  .fromTo(
-    ".logo",
-    { x: "-50%", opacity: 0 },
-    { x: 0, opacity: 1, duration: 2.5 }
-  )
-  .fromTo(
-    ".infographi-text",
-    { y: "50%", opacity: 0 },
-    { y: 0, opacity: 1, duration: 2.5 },
-    "<"
-  )
-  .fromTo(
-    ".Kategori",
-    { y: "-50%", opacity: 0 },
-    { y: 0, opacity: 1, duration: 2.5 },
-    "<"
-  )
-  .fromTo(
-    ".burger",
-    { y: "-50%", opacity: 0 },
-    { y: 0, opacity: 1, duration: 2.5 },
-    "<"
-  );
+const matchMediaResponsive = gsap.matchMedia();
 
-// quotes sections
-
-const tlQuotesSections = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#about",
-    start: "-25% center",
-    end: "0% top",
-    duration: 0.75,
-    scrub: 4,
+matchMediaResponsive.add(
+  {
+    isDesktopTab: "(min-width: 721px)",
+    isMobile: "(max-width: 720px)",
   },
-});
+  (context) => {
+    console.log(context.conditions);
+    const { isMobile, isDesktopTab } = context.conditions;
 
-tlQuotesSections
-  .fromTo(".quotes2", { y: "50%", opacity: 0 }, { y: 0, opacity: 1 })
-  .fromTo(".quotes3", { x: "50%", opacity: 0 }, { x: 0, opacity: 1 }, "<35%")
-  .fromTo(".quotes1", { x: "-50%", opacity: 0 }, { x: 0, opacity: 1 }, "<35%")
-  .fromTo(".about-container img", { opacity: 0 }, { opacity: 1 }, "<35%");
+    const tl = gsap.timeline({
+      default: { duration: 0.75, ease: "Power3.easeOut" },
+    });
 
-// kategori sections -----------------------------------------------------
+    // navbar -----------------------------------------------------
 
-const tlKategoriSections = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#kategori",
-    start: "-25% center",
-    end: "0% top",
-    duration: 0.75,
-    scrub: 4,
-  },
-});
+    const showAnim = gsap
+      .from("nav", {
+        yPercent: -100,
+        paused: true,
+        duration: 0.2,
+      })
+      .progress(1);
 
-tlKategoriSections
-  .fromTo(".cta1-title1", { y: "-50%", opacity: 0 }, { y: 0, opacity: 1 })
-  .fromTo(
-    ".cta2-title2",
-    { y: "50%", opacity: 0 },
-    { y: 0, opacity: 1 },
-    "<35%"
-  )
-  .fromTo(".sub-title1", { y: "50%", opacity: 0 }, { y: 0, opacity: 1 }, "<35%")
-  .fromTo(".kategori", { opacity: 0 }, { opacity: 1, duration: 4 }, "<35%");
+    ScrollTrigger.create({
+      start: "top top",
+      end: 99999,
+      onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse();
+      },
+    });
 
-// penutup sections ---------------------------------------------------------------
-// --------------------------------------------------------------------------------
+    // home sections
 
-const tlPenutupSections = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#penutup",
-    start: "-25% center",
-    end: "0% top",
-    duration: 0.75,
-    scrub: 4,
-  },
-});
+    tl.fromTo(
+      ".cta-home1",
+      { y: "-50%", opacity: 0 },
+      { y: 0, opacity: 1, duration: 1.5 }
+    )
+      .fromTo(
+        ".cta-home2",
+        { y: "50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<35%"
+      )
+      .fromTo(
+        ".cta-home3",
+        { x: "50%", opacity: 0 },
+        { x: 0, opacity: 1, duration: 1.5 },
+        "<35%"
+      )
+      .fromTo(
+        ".cta-button",
+        { x: "50%", opacity: 0 },
+        { x: 0, opacity: 1, duration: 2.5 },
+        "<35%"
+      )
+      .fromTo(
+        ".logo",
+        { x: "-50%", opacity: 0 },
+        { x: 0, opacity: 1, duration: 1.5 }
+      )
+      .fromTo(
+        ".infographi-text",
+        { y: "50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<"
+      )
+      .fromTo(
+        ".Kategori",
+        { y: "-50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<"
+      )
+      .fromTo(
+        ".youtube-icon",
+        { y: "-50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<"
+      )
+      .fromTo(
+        ".instagram-icon",
+        { y: "-50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<"
+      )
+      .fromTo(
+        ".burger",
+        { y: "-50%", opacity: 0 },
+        { y: 0, opacity: 1, duration: 1.5 },
+        "<"
+      );
 
-tlPenutupSections
-  .fromTo(
-    ".title-penutup",
-    { x: "-50%", opacity: 0 },
-    { x: "0", opacity: 1, duration: 4 }
-  )
-  .fromTo(
-    ".sub-penutup",
-    { y: "50%", opacity: 0 },
-    { y: "0", opacity: 1, duration: 4 },
-    "<35%"
-  )
-  .fromTo(
-    ".youtube",
-    { scale: 0 },
-    { scale: 1, yoyo: true, duration: 4 },
-    "<35%"
-  )
-  .fromTo(
-    ".instagram",
-    { scale: 0 },
-    { scale: 1, yoyo: true, duration: 4 },
-    "<35%"
-  );
+    // quotes sections
 
-// social media animation ---------------------------------------------------------
-// --------------------------------------------------------------------------------
+    const tlQuotesSections = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "-25% center",
+        end: "0% top",
+        duration: 0.75,
+        scrub: 4,
+      },
+    });
 
-const youtube = document.querySelector(".youtube");
-const instagram = document.querySelector(".instagram");
+    tlQuotesSections
+      .fromTo(
+        ".quotes2",
+        { y: isMobile ? 0 : "50%", opacity: isMobile ? 1 : 0 },
+        { y: 0, opacity: 1 }
+      )
+      .fromTo(
+        ".quotes3",
+        { x: isMobile ? 0 : "50%", opacity: isMobile ? 1 : 0 },
+        { x: 0, opacity: 1 },
+        "<35%"
+      )
+      .fromTo(
+        ".quotes1",
+        { x: isMobile ? 0 : "-50%", opacity: isMobile ? 1 : 0 },
+        { x: 0, opacity: 1 },
+        "<35%"
+      )
+      .fromTo(
+        ".about-container img",
+        { opacity: isMobile ? 1 : 0 },
+        { opacity: 1 },
+        "<35%"
+      );
 
-youtube.addEventListener("click", () => {
-  gsap.fromTo(".youtube", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
-});
+    // kategori sections -----------------------------------------------------
 
-instagram.addEventListener("click", () => {
-  gsap.fromTo(
-    ".instagram",
-    { scale: 1 },
-    { scale: 0.9, yoyo: true, repeat: 1 }
-  );
-});
+    const tlKategoriSections = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#kategori",
+        start: "-25% center",
+        end: "0% top",
+        duration: 0.75,
+        scrub: 4,
+      },
+    });
 
-// footer closed animation ---------------------------------------------------------
-// --------------------------------------------------------------------------------
+    tlKategoriSections
+      .fromTo(".cta1-title1", { y: "-50%", opacity: 0 }, { y: 0, opacity: 1 })
+      .fromTo(
+        ".cta2-title2",
+        { y: "50%", opacity: 0 },
+        { y: 0, opacity: 1 },
+        "<35%"
+      )
+      .fromTo(
+        ".sub-title1",
+        { y: "50%", opacity: 0 },
+        { y: 0, opacity: 1 },
+        "<35%"
+      )
+      .fromTo(
+        ".kategori",
+        { opacity: isMobile ? 1 : 0 },
+        { opacity: 1, duration: 4 },
+        "<35%"
+      );
 
-const tlFooter = gsap.timeline({
-  scrollTrigger: {
-    trigger: "body",
-    start: "80% center",
-    end: "80% center",
-    duration: 0.75,
-    scrub: 4,
-  },
-});
+    // penutup sections ---------------------------------------------------------------
 
-tlFooter.fromTo(".footer", { y: "100%" }, { y: 0 });
+    const tlPenutupSections = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#penutup",
+        start: "-25% center",
+        end: "0% top",
+        duration: 0.75,
+        scrub: 4,
+      },
+    });
+
+    tlPenutupSections
+      .fromTo(
+        ".title-penutup",
+        { x: isMobile ? "0" : "-50%", opacity: isMobile ? 1 : 0 },
+        { x: "0", opacity: 1, duration: 4 }
+      )
+      .fromTo(
+        ".sub-penutup",
+        { y: isMobile ? "0" : "50%", opacity: isMobile ? 1 : 0 },
+        { y: "0", opacity: 1, duration: 4 },
+        "<35%"
+      );
+
+    // footer closed animation ---------------------------------------------------------
+
+    const tlFooter = gsap.timeline({
+      scrollTrigger: {
+        trigger: "body",
+        start: "80% center",
+        end: "80% center",
+        duration: 0.75,
+        scrub: 4,
+      },
+    });
+
+    tlFooter.fromTo(".footer", { y: "100%" }, { y: 0 });
+  }
+);
